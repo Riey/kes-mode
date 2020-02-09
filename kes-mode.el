9ed6f52374b98ad8361c855417ec1b7cc0892f30
Riey <creeper844@gmail.com>
2020-02-09 14:22:45 +0900

    Update language

--- kes-mode.el
+++ kes-mode.el
@@ -1,3 +1,5 @@
+(eval-when-compile (require 'rx))
+
 (defvar kes-mode-hook nil)
 
 (defface kes-operator-face
@@ -8,18 +10,15 @@
 (defconst kes-keywords
   '("그외" "선택"))
 
-(defconst kes-builtins
-  '("랜덤" "이름" "주인이름" "호칭" "주인호칭" "애칭" "주인애칭" "별명" "주인별명" "연모" "음란" "복종" "키스한다" "이전커맨드" "처음" "키스한다" "굴복각인"))
-
 (defconst kes-operators
-  '("/" "&" "|" "\\^" "*" "+" "%" "=" "!" "<" ">"))
+  '("/" "&" "|" "\\^" "*" "+" "%" "-" "<" ">" "#" "@" "[" "]" "?"))
 
 (defconst kes-font-lock-keywords
   `(
     (,(regexp-opt kes-keywords  'symbols) . font-lock-keyword-face)
-    (,(regexp-opt kes-builtins  'symbols) . font-lock-builtin-face)
+    (,(rx (in "ㄱ-ㅎㅏ-ㅣ가-힣_") (* (in "0-9ㄱ-ㅎㅏ-ㅣ가-힣_"))) (0 font-lock-builtin-face))
     (,(regexp-opt kes-operators) . 'kes-operator-face)
-    (,(rx (group "$") (group (+ (in "0-9ㄱ-ㅎㅏ-ㅣ가-힣_")))) (0 'kes-operator-face) (1 font-lock-variable-name-face))))
+    (,(rx "$" (+ (in "0-9ㄱ-ㅎㅏ-ㅣ가-힣_"))) (0 font-lock-variable-name-face))))
 
 (defvar kes-mode-syntax-table
   (let ((st (make-syntax-table)))
@@ -32,9 +31,11 @@
     (modify-syntax-entry ?+  "." st)
     (modify-syntax-entry ?=  "." st)
     (modify-syntax-entry ?%  "." st)
-    (modify-syntax-entry ?!  "." st)
+    (modify-syntax-entry ?@  "." st)
+    (modify-syntax-entry ?#  "." st)
     (modify-syntax-entry ?<  "." st)
     (modify-syntax-entry ?>  "." st)
+    (modify-syntax-entry ?\?  "." st)
 
     ;; Comment
     (modify-syntax-entry ?/  ". 124b" st)
@@ -42,13 +43,12 @@
 
     ;; String quote
     (modify-syntax-entry ?'  "\"'" st)
-    (modify-syntax-entry ?{  "(} \\}")
 
     ;; Brackets
     (modify-syntax-entry ?{  "(}" st)
     (modify-syntax-entry ?}  "){" st)
-    (modify-syntax-entry ?\(  "()" st)
-    (modify-syntax-entry ?\)  ")(" st)
+    (modify-syntax-entry ?\[  "(]" st)
+    (modify-syntax-entry ?\]  ")[" st)
 
     st))
 
@@ -61,10 +61,13 @@
   (setq-local font-lock-defaults '(kes-font-lock-keywords))
   (setq-local comment-start-skip "\\(?://[/!]*\\|/\\*[*!]?\\)[[:space:]]*")
   (setq-local comment-multi-line nil)
-  (setq-local
-   electric-indent-chars (cons ?} (and (boundp 'electric-indent-chars)
-                                       electric-indent-chars))))
+  (setq-local electric-indent-mode t)
+  (setq-local electric-indent-chars (list ?\n ?}))
+  (setq-local tab-width 4)
+  (defvaralias 'c-basic-offset 'tab-width)
+  (defvaralias 'cperl-indent-level 'tab-width))
 
+;;;###autoload
 (add-to-list 'auto-mode-alist '("\\.kes\\'" . kes-mode))
 
 (provide 'kes-mode)
